2b2e084358404cf2d85b4a5ea8e0a616538c1ec7
Luigui de A <luisguilhermepdea@gmail.com>
2021-01-10 19:51:24 -0300

    day_3 solved

--- day_3/src/day_3/core.clj
+++ day_3/src/day_3/core.clj
@@ -1,7 +1,40 @@
 (ns day-3.core
+  (:require [aoc-2015.core :as aoc]
+            [clojure.string :as str])
   (:gen-class))
 
+(def move-map {"^" [0 1] ">" [1 0] "v" [0 -1] "<" [-1 0]})
+
+(defn add-move [coordinates raw-move]
+  (let [move (get move-map raw-move)]
+    (->> move
+         (map + (last coordinates))
+         (conj coordinates))))
+
+(defn add-all-houses [raw-moves]
+  (reduce add-move [[0 0]] raw-moves))
+
+
+(defn add-all-houses-2 [raw-moves]
+  (concat (add-all-houses (take-nth 2 raw-moves))
+          (add-all-houses (take-nth 2 (rest raw-moves)))))
+
+(defn solve []
+  (-> (aoc/get-challenge 3)
+      (str/split #"")
+      add-all-houses
+      set
+      count))
+
+(defn solve2 []
+  (-> (aoc/get-challenge 3)
+      (str/split #"")
+      add-all-houses-2
+      set
+      count))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (solve))
+  (println (solve2)))
