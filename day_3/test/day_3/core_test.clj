0016c498d0695662f91c0aa085e20a6484c36eb9
Luigui de A <luisguilhermepdea@gmail.com>
2021-01-10 19:51:24 -0300

    day_3 solved

--- day_3/test/day_3/core_test.clj
+++ day_3/test/day_3/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [day-3.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def one-move-up [[0 0] [0 1]])
+(def raw "^><<v")
+(def moves ["^" ">" "<" "<" "v" "<" "<" "<"])
+(def expected 2081)
+(def expected2 2341)
+
+(deftest add-move-test
+  (testing "add-move-test"
+    (is (= one-move-up (add-move [[0 0]] "^")))))
+
+(deftest add-move-test2
+  (testing "add-move-test2"
+    (is (= [[0 0] [0 1] [0 2]] (add-move one-move-up "^")))))
+
+(deftest  solve-test
+  (testing "solve-test"
+    (is (= expected (solve)))))
+
+(deftest  solve-test-2
+  (testing "solve-test"
+    (is (= expected2 (solve2)))))
