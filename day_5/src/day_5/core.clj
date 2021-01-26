93e5a25445bad8a3384f8234d186f9beb4bfdd31
Luigui de A <luisguilhermepdea@gmail.com>
2021-01-26 16:53:34 -0300

    day_5 solved

--- day_5/src/day_5/core.clj
+++ day_5/src/day_5/core.clj
@@ -1,10 +1,54 @@
 (ns day-5.core
   (:gen-class)
-  (:require [aoc-2015.core :as aoc]))
+  (:require [aoc-2015.core :as aoc]
+            [clojure.string :as str]))
 
-(defn solve []
-       (-> (aoc/get-challenge 5)))
+(defn at-least-3-vowels [word]
+                   (<= 3
+                       (->> word
+                            (re-seq #"[aeiou]")
+                            count)))
+
+(defn a-double-letter [word]
+  (some?
+    (re-seq #"(\w)\1" word)))
+
+(defn disallowed-words-free [word]
+  (nil?
+    (some true?
+          (map #(str/includes? word %) ["ab" "cd" "pq" "xy"]))))
+
+(defn pair-that-appears-at-least-two [word]
+  (some?
+    (re-seq #"(\w\w)\w*\1" word)))
+
+(defn one-letter-between-same-letter [word]
+  (some?
+    (re-seq #"(\w)\w\1" word)))
+
+(defn is-nice? [word]
+  (and
+    (at-least-3-vowels word)
+    (a-double-letter word)
+    (disallowed-words-free word)))
+
+(defn is-nice2? [word]
+  (and
+    (pair-that-appears-at-least-two word)
+    (one-letter-between-same-letter word)))
+
+(defn filter-nice [word-list is-nice?]
+  (filter is-nice? word-list))
+
+(defn solve [is-nice?]
+  (-> (aoc/get-challenge 5)
+      (str/split #"\n")
+      (filter-nice is-nice?)))
+
+(defn save-solution []
+  (spit "resources/solutionclj.txt" (str/join "\n" (solve))))
 
 (defn -main
   [& args]
-  (println (solve)))
+  (println (count (solve is-nice?)))
+  (println (count (solve is-nice2?))))
--- day_5/test/day_5/core_test.clj
+++ day_5/test/day_5/core_test.clj
@@ -2,6 +2,35 @@
   (:require [clojure.test :refer :all]
             [day-5.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest at-least-3-vowels-cases
+  (testing "at-least-3-vowels-testcases"
+    (is (at-least-3-vowels "aei"))
+    (is (false? (at-least-3-vowels "xyz")))))
+
+(deftest double-letter-cases
+  (testing "double-letter"
+    (is (a-double-letter "xxyyzz"))
+    (is (false? (a-double-letter "xyz")))
+    (is (false? (a-double-letter "iylmzraibygmgmqj")))))
+
+(deftest disallowed-words-cases
+  (testing "disallowed-words-true"
+    (is (disallowed-words-free "afb"))
+    (is (false? (disallowed-words-free "ab")))))
+
+(deftest filter-nice-true
+  (testing "filter-nice"
+    (is (= 4
+           (count (filter-nice ["ab" "aaei" "xxa" "chrigojuzz" "ugknbfddgicrmopn" "aaa" "haegwjzuvuyypxyu
+           'jchzalrnumimnmhp" "dvszwmarrgswjxmb" "iylmzraibygmgmqj"]))))))
+
+(deftest pair-that-appears-at-least-two-cases
+  (testing "pair-that-appears-at-least-two cases"
+    (is (pair-that-appears-at-least-two "aabcdefgaa"))
+    (is (false? (pair-that-appears-at-least-two "abcdefgaa")))))
+
+(deftest one-letter-between-same-letter-cases
+  (testing "one-letter-between-same-letter cases"
+    (is (one-letter-between-same-letter "aya"))
+    (is (one-letter-between-same-letter "aaa"))
+    (is (false? (one-letter-between-same-letter "abcdefgaa")))))
