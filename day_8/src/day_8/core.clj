a49bee35a17e81438e6a696d21e8c7e4dec28a0d
Luigui de A <luisguilhermepdea@gmail.com>
2021-02-19 08:24:24 -0300

    day 8 part 1 fix needed

--- day_8/src/day_8/core.clj
+++ day_8/src/day_8/core.clj
@@ -1,5 +1,49 @@
 (ns day-8.core
+  (:require [clojure.string  :as str]
+            [aoc-2015.core :as aoc])
   (:gen-class))
+
+(def escaped-re #"[\"\\]")
+(def escaped-cn {:ns 2 :ms 1})
+(def hexa-re #"\\x[0-9a-f]{2}")
+(def hexa-cn {:ns 4 :ms 1})
+(def quotes-re #"(^\"|\"$)")
+(def quotes-cn {:ns 1 :ms 0})
+
+(defn calculate [re cn m]
+  (loop [{s :s cns :cns} m ]
+    (if (nil? (re-find re s))
+      {:s s :cns cns}
+      (recur {:s (str/replace-first s re "")
+              :cns (conj cns cn)}))))
+
+(defn calculate-remain [{s :s cns :cns}]
+  (->> s
+      count
+      ((fn [r] {:ns r :ms r}))
+      (conj cns)))
+
+(defn sum-maps [{tns :ns tms :ms} {ns :ns ms :ms}]
+  {:ns (+ tns ns) :ms (+ tms ms)})
+
+(defn calculate-all [m]
+  (->> m
+      (calculate quotes-re quotes-cn)
+      (calculate hexa-re hexa-cn)
+      (calculate escaped-re escaped-cn)
+      calculate-remain
+      (reduce sum-maps)))
+
+(defn txt->str-list []
+  (->
+   (aoc/get-challenge 8)
+   (str/split #"\n")))
+
+(defn solve1 []
+  (->> (txt->str-list)
+       (map (fn [s] {:s s :cns []}))
+       (map calculate-all)
+       (reduce sum-maps)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- day_8/test/day_8/core_test.clj
+++ day_8/test/day_8/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [day-8.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest calculate-test
+  (is (= {:s "abab" :cns (repeat 2 escaped-cn)}
+         (calculate escaped-re escaped-cn {:s "ab\"\"ab" :cns []})))
+  (is (= {:s "abab" :cns (repeat 2 quotes-cn)}
+         (calculate quotes-re quotes-cn {:s "\"abab\"" :cns []})))
+  (is (= {:s "abab" :cns [hexa-cn]}
+         (calculate hexa-re hexa-cn {:s "ab\\xabab" :cns []}))))
+
+(deftest calculate-remain-test
+  (is (= [escaped-cn hexa-cn {:ns 4 :ms 4}]
+         (calculate-remain {:s "abab" :cns [escaped-cn hexa-cn]}))))
+
+(deftest calculate-all-test
+  (is (= {:ns 14 :ms 7} (calculate-all {:s "\"a\\b\"a\\x32b\"" :cns []}))))
